0e8839defd3d37bcd81591622c02bf2cf49ccf45
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-29 12:19:17 +0600

    Footer

--- footer.php
+++ footer.php
@@ -11,22 +11,41 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ibrahimportfolio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ibrahimportfolio' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ibrahimportfolio' ), 'ibrahimportfolio', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="footer py-3">
+			<div class="container">
+				<p class="small mb-0 text-light">
+					&copy; <script>document.write(new Date().getFullYear())</script> Created With <i class="ti-heart text-danger"></i> By <a href="http://devcrud.com" target="_blank"><span class="text-danger" title="Bootstrap 4 Themes and Dashboards">DevCRUD</span></a> 
+				</p>
+			</div>
+		</footer>
+
+		<!-- core  -->
+		<?php wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/vendors/jquery/jquery-3.4.1.js', null, null, true); ?>
+		<?php wp_enqueue_script( 'mainbs', get_template_directory_uri() . '/assets/vendors/bootstrap/bootstrap.bundle.js', null, null, true); ?>
+		<!-- <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+		<script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script> -->
+
+		<!-- bootstrap 3 affix -->
+		<?php wp_enqueue_script( 'bsaffix', get_template_directory_uri() . '/assets/vendors/bootstrap/bootstrap.affix.js', null, null, true); ?>
+		<!-- <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script> -->
+
+		<!-- Isotope  -->
+		<?php wp_enqueue_script( 'bsisotope', get_template_directory_uri() . '/assets/vendors/isotope/isotope.pkgd.js', null, null, true); ?>
+		<!-- <script src="assets/vendors/isotope/isotope.pkgd.js"></script> -->
+		
+		<!-- Google mpas -->
+		<?php
+            wp_register_script( 'mapscript', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap' );
+            wp_enqueue_script('mapscript');
+		?>
+		<!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script> -->
+
+		<!-- JohnDoe js -->
+		<?php wp_enqueue_script( 'jdjs', get_template_directory_uri() . '/assets/js/johndoe.js', null, null, true); ?>
+		<!-- <script src="assets/js/johndoe.js"></script> -->
+
+	</body>
+	</html>
 
 <?php wp_footer(); ?>
 
--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package ibrahimportfolio
- */
 
 get_header();
 ?>
@@ -377,33 +365,6 @@ get_header();
 
 			</div>
 		</div>
-		<footer class="footer py-3">
-			<div class="container">
-				<p class="small mb-0 text-light">
-					&copy; <script>document.write(new Date().getFullYear())</script> Created With <i class="ti-heart text-danger"></i> By <a href="http://devcrud.com" target="_blank"><span class="text-danger" title="Bootstrap 4 Themes and Dashboards">DevCRUD</span></a> 
-				</p>
-			</div>
-		</footer>
-
-		<!-- core  -->
-		<script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
-		<script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
-
-		<!-- bootstrap 3 affix -->
-		<script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
-
-		<!-- Isotope  -->
-		<script src="assets/vendors/isotope/isotope.pkgd.js"></script>
-		
-		<!-- Google mpas -->
-		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script>
-
-		<!-- JohnDoe js -->
-		<script src="assets/js/johndoe.js"></script>
-
-	</body>
-	</html>
-
 
 <?php
 	get_sidebar();
