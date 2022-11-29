fe3134fe2abbdc3c317db9efa00c6a9eb2810f14
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2022-11-29 11:36:08 +0600

    Details Theme Part

--- index.php
+++ index.php
@@ -36,171 +36,18 @@ get_header();
 		?>
 		<!-- About End -->
 
-
 		<!--Resume Section-->
-		<section class="section" id="resume">
-			<div class="container">
-				<h2 class="mb-5"><span class="text-danger">My</span> Resume</h2>
-				<div class="row">
-					<div class="col-md-6 col-lg-4">
-						<div class="card">
-						<div class="card-header">
-								<div class="mt-2">
-									<h4>Expertise</h4>
-									<span class="line"></span>  
-								</div>
-							</div>
-							<div class="card-body">
-								<h6 class="title text-danger">2017 - Present</h6>
-								<P>UX Developer</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum recusandae, cupiditate ullam dolor ratione repellendus.aliquid repudiandae saepe!.</P>
-								<hr>
-								<h6 class="title text-danger">2016 - 2017</h6>
-								<P>Front-end Developer</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum recusandae, cupiditate ullam dolor ratione repellendus.aliquid repudiandae saepe!.</P>
-								<hr>
-								<h6 class="title text-danger">2015 - 2016</h6>
-								<P>UX Designer</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum recusandae, cupiditate ullam dolor ratione repellendus.aliquid repudiandae saepe!.</P>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-4">
-						<div class="card">
-						<div class="card-header">
-								<div class="mt-2">
-									<h4>Education</h4>
-									<span class="line"></span>  
-								</div>
-							</div>
-							<div class="card-body">
-								<h6 class="title text-danger">2017 - Present</h6>
-								<P>B.E Computer Engineering</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error corrupti recusandae obcaecati odit repellat ducimus cum, minus tempora aperiam at.</P>
-								<hr>
-								<h6 class="title text-danger">2016 - 2017</h6>
-								<P>Diploma in Computer Engineering</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, id officiis quas placeat quia voluptas dolorum rem animi nostrum quae.aliquid repudiandae saepe!.</P>
-								<hr>
-								<h6 class="title text-danger">2015 - 2016</h6>
-								<P>High School Degree</P>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum recusandae, cupiditate ullam dolor ratione repellendus.aliquid repudiandae saepe!.</P>
-								
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4">
-						<div class="card">
-						<div class="card-header">
-								<div class="pull-left">
-									<h4 class="mt-2">Skills</h4>
-									<span class="line"></span>  
-								</div>
-							</div>
-							<div class="card-body pb-2">
-							<h6>hTL5 &amp; CSS3</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 97%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>JavaScript</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 85%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>PHP</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>SQL</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>Laborum</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>Tempora</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-							</div>
-						</div>
-						<div class="card">
-						<div class="card-header">
-								<div class="pull-left">
-									<h4 class="mt-2">Languages</h4>
-									<span class="line"></span>  
-								</div>
-							</div>
-							<div class="card-body pb-2">
-							<h6>English</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>French</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 45%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-								<h6>Spanish</h6>
-								<div class="progress mb-3">
-									<div class="progress-bar bg-danger" role="progressbar" style="width: 67%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<?php
+			get_template_part('template-parts/content', 'resume');
+		?>
+		<!--Resume Section End-->
 
-		<section class="section bg-dark text-center">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-md-6 col-lg-3">
-						<div class="row ">
-							<div class="col-5 text-right text-light border-right py-3">
-								<div class="m-auto"><i class="ti-alarm-clock icon-xl"></i></div>
-							</div>
-							<div class="col-7 text-left py-3">
-								<h1 class="text-danger font-weight-bold font40">500</h1>
-								<p class="text-light mb-1">Hours Worked</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3">
-						<div class="row">
-							<div class="col-5 text-right text-light border-right py-3">
-								<div class="m-auto"><i class="ti-layers-alt icon-xl"></i></div>
-							</div>
-							<div class="col-7 text-left py-3">
-								<h1 class="text-danger font-weight-bold font40">50K</h1>
-								<p class="text-light mb-1">Project Finished</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3">
-						<div class="row">
-							<div class="col-5 text-right text-light border-right py-3">
-								<div class="m-auto"><i class="ti-face-smile icon-xl"></i></div>
-							</div>
-							<div class="col-7 text-left py-3">
-								<h1 class="text-danger font-weight-bold font40">200K</h1>
-								<p class="text-light mb-1">Happy Clients</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3">
-						<div class="row">
-							<div class="col-5 text-right text-light border-right py-3">
-								<div class="m-auto"><i class="ti-heart-broken icon-xl"></i></div>
-							</div>
-							<div class="col-7 text-left py-3">
-								<h1 class="text-danger font-weight-bold font40">2k</h1>
-								<p class="text-light mb-1">Coffee Drinked</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<!--Details Section-->
+		<?php
+			get_template_part('template-parts/content', 'details');
+		?>
+		<!--Details Section End-->
+
 
 		<section class="section" id="service">
 			<div class="container">
