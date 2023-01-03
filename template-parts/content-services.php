07e026c9aef0768cc09fa854d39a56e47c9e3858
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2023-01-04 00:22:25 +0600

    Dynamic Fields Creation

--- template-parts/content-services.php
+++ template-parts/content-services.php
@@ -6,39 +6,28 @@
 			?>
 				<h2 class="mb-5 pb-4"><span class="text-danger"><?php echo $string_converter_services_heading['opt-services-heading1']; ?></span> <?php echo $string_converter_services_heading['opt-services-heading2']; ?></h2>
 				<div class="row">
-					<div class="col-md-4 col-sm-6">
-						<div class="card mb-5">
-						<div class="card-header has-icon">
-						<!-- Services Details Loop -->
+					<!-- Services Details Loop -->
 							<?php
 								if($string_converter_services_heading['opt-services-group']){
 									foreach($string_converter_services_heading['opt-services-group'] as $single_array_convert_services){
-										// var_dump($single_array_convert_lan);
+										var_dump($single_array_convert_services);
 										?>
-											<i class="<?php echo $single_array_convert_services['opt-services-icon-class']; ?>" aria-hidden="true"></i>
-											</div>
-											<div class="card-body px-4 py-3">
-												<h5 class="mb-3 card-title text-dark"><?php echo $single_array_convert_services['opt-languages-title']; ?></h5>
-												<P class="subtitle"><?php echo $single_array_convert_services['opt-languages-title']; ?></P>
+											<div class="col-md-4 col-sm-6">
+												<div class="card mb-5">
+												<div class="card-header has-icon">
+														<i class="<?php echo $single_array_convert_services['opt-services-icon-class']; ?>" aria-hidden="true"></i>
+													</div>
+													<div class="card-body px-4 py-3">
+														<h5 class="mb-3 card-title text-dark"><?php echo $single_array_convert_services['opt-services-title']; ?></h5>
+														<P class="subtitle"><?php echo $single_array_convert_services['opt-services-details']; ?></P>
+													</div>
+												</div>
 											</div>
 										<?php
 									}
 								}
 							?>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6">
-						<div class="card mb-5">
-						<div class="card-header has-icon">
-								<i class="ti-write text-danger" aria-hidden="true"></i>
-							</div>
-							<div class="card-body px-4 py-3">
-								<h5 class="mb-3 card-title text-dark">Asperiores</h5>
-								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam commodi provident, dolores reiciendis enim pariatur error optio, tempora ex, nihil nesciunt! In praesentium sunt commodi, unde ipsam ex veritatis laboriosam dolor asperiores suscipit blanditiis, dignissimos quos nesciunt nulla aperiam officia.</P>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6">
+					<!-- <div class="col-md-4 col-sm-6">
 						<div class="card mb-5">
 						<div class="card-header has-icon">
 								<i class="ti-package text-danger" aria-hidden="true"></i>
@@ -81,7 +70,7 @@
 								<P class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam commodi provident, dolores reiciendis enim pariatur error optio, tempora ex, nihil nesciunt! In praesentium sunt commodi, unde ipsam ex veritatis laboriosam dolor asperiores suscipit blanditiis, dignissimos quos nesciunt nulla aperiam officia.</P>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</section>
